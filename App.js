6f708979cbb9ce911ce37325896e617ddf4e39b1
sunksun <sunksunlapunt@gmail.com>
2024-06-24 15:01:48 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {StyleSheet, View, Image, Text} from 'react-native';
+
+const Flex = () => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={{flex: 2, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'}}>
+        <Image style={{width:120, height:160}}
+          source={require('./assets/lru.png')}
+        />
+      </View>
+      <View style={{flex: 1, backgroundColor: 'darkorange'}}>
+        <Text style={{fontSize: 24}}>มหาวิทยาลัยราชภัฏเลย</Text>
+      </View>
+      <View style={{flex: 3, backgroundColor: 'green'}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
+
+export default Flex;
